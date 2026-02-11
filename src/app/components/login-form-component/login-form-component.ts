bb11cebac688d0b20f5cfed586d5c1987002ba3b
Henrique Simon <henriquesimon1998@gmail.com>
2026-02-11 20:58:23 -0300

    feat: feat: add login form with validations

--- src/app/components/login-form-component/login-form-component.ts
+++ src/app/components/login-form-component/login-form-component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-login-form-component',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-form-component.html',
   styleUrl: './login-form-component.scss',
 })
 export class LoginFormComponent {
+  form = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
 
+  onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+  }
 }
